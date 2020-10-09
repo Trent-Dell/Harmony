bcfacee9a1b971b1ad68faed08d865e29365b91c
TrentBrunson <66187531+TrentBrunson@users.noreply.github.com>
2020-10-09 11:26:35 -0500

    excel load

--- aSkuValidate.py
+++ aSkuValidate.py
@@ -11,6 +11,7 @@ from config import db_password
 # load data files
 a? = "data\parts_arev_na.txt"
 b? = "data\parts_xrev_na.txt"
+
 #%%
 # load 3 sets rev into dataframe
 df = pd.read_csv(arev_load, sep=',')
